6426aa3afd2742db269eba34a0bc7bd5cac9d8e5
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-05-04 23:34:12 +0100

    I setup the Edit route and functionalityin in the blog. Next i have to deal with markdown

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -19,22 +19,24 @@ router.get("/new", function (req, res) {
   });
 });
 
-router.post("/", async function (req, res) {
-  let post = new Post({
-    title: req.body.title,
-    description: req.body.description,
-    author: req.body.author,
-    markdown: req.body.article,
-  });
+router.post(
+  "/",
+  async function (req, res, next) {
+    req.post = new Post();
+    next();
+  },
+  savePostandRedirect("newPost")
+);
 
-  try {
-    post = await post.save();
-    res.redirect(`/posts/${post.id}`);
-  } catch (err) {
-    debug(err);
-    res.render("newPost", { post: post });
-  }
-});
+router.put(
+  "/:id",
+  async function (req, res, next) {
+    let id = req.params.id.trim();
+    req.post = await Post.findById(id);
+    next();
+  },
+  savePostandRedirect("editPost")
+);
 
 router.delete("/:id", async function (req, res) {
   Post.findByIdAndDelete(req.params.id)
@@ -45,4 +47,37 @@ router.delete("/:id", async function (req, res) {
       console.log(error);
     });
 });
+
+router.get("/edit/:id", async function (req, res) {
+  let post = await Post.findById(req.params.id);
+  try {
+    res.render("editPost", {
+      title: "Edit Post - Onoh Somtochukwu",
+      post: post,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+function savePostandRedirect(file) {
+  return async (req, res) => {
+    let post = req.post;
+
+    post.title = req.body.title;
+    post.description = req.body.description;
+    post.author = req.body.author;
+    post.markdown = req.body.article;
+
+    try {
+      post = await post.save();
+
+      res.redirect(`/posts/${post.id}`);
+    } catch (err) {
+      debug(err);
+      res.render(`${file}`, { post: post });
+    }
+  };
+}
+
 module.exports = router;
