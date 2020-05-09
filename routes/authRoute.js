125e7d0e9e4f335e5839b89b226def53cfc8e008
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-05-09 13:59:47 +0100

    Completed all the logics for registering an admin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-// const Author = require("./../models/author");
+const Admin = require("../models/admin");
+const bcrypt = require("bcrypt");
 
 router.get("/new", async function (req, res) {
   res.render("newAdmin", {
@@ -8,9 +9,21 @@ router.get("/new", async function (req, res) {
   });
 });
 
-router.delete("/:id", async function (req, res) {
-  await Post.findByIdAndDelete(req.params.id);
-  res.redirect("/admin/");
+router.post("/", async function (req, res) {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    let admin = new Admin({
+      name: req.body.name,
+      password: hashedPassword,
+    });
+
+    let result = await admin.save();
+    console.log(result);
+    res.redirect("/auth/login");
+  } catch (e) {
+    res.redirect("/auth/new");
+  }
 });
 
 module.exports = router;
