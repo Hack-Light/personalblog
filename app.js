368b9ff764809d7ab305d7d45c3cfa1018457154
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-05-12 01:53:27 +0100

    Fixing Errors from deployment

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use("/about", aboutRouter);
 //   res.render("error");
 // });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("listening on port 3000");
 });
 
